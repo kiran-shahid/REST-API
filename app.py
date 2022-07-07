61a2070eb83807687a20bddbe858e24821b90cc2
kiran-shahid <kirans2019@namal.edu.pk>
2022-07-07 17:36:26 +0500

    commit

---
+++ app.py
@@ -0,0 +1,141 @@
+import os
+from flask import Flask, jsonify, request, json,render_template, url_for
+
+app = Flask(__name__)
+
+
+data = [
+  {
+    "country": "England",
+    "team-matches-played": 746,
+    "matches-won": 375,
+    "matches-lost": 334
+  },
+  {
+    "country": "Australia",
+    "team-matches-played": 949,
+    "matches-won": 575,
+    "matches-lost": 331
+  },
+  {
+    "country": "South Africa",
+    "team-matches-played": 625,
+    "matches-won": 385,
+    "matches-lost": 216
+  },
+  {
+    "country": "West Indies",
+    "team-matches-played": 822,
+    "matches-won": 401,
+    "matches-lost": 381
+  },
+  {
+    "country": "New Zealand",
+    "team-matches-played": 772,
+    "matches-won": 351,
+    "matches-lost": 374
+  },
+  {
+    "country": "India",
+    "team-matches-played": 987,
+    "matches-won": 513,
+    "matches-lost": 424
+  },
+  {
+    "country": "Pakistan",
+    "team-matches-played": 927,
+    "matches-won": 486,
+    "matches-lost": 413
+  },
+  {
+    "country": "Sri Lanka",
+    "team-matches-played": 852,
+    "matches-won": 389,
+    "matches-lost": 421
+  },
+  {
+    "country": "Zimbabwe",
+    "team-matches-played": 529,
+    "matches-won": 138,
+    "matches-lost": 373
+  },
+  {
+    "country": "Bermuda",
+    "team-matches-played": 35,
+    "matches-won": 7,
+    "matches-lost": 28
+  },
+  {
+    "country": "Netherlands",
+    "team-matches-played": 80,
+    "matches-won": 31,
+    "matches-lost": 45
+  },
+  {
+    "country": "Bangladesh",
+    "team-matches-played": 376,
+    "matches-won": 128,
+    "matches-lost": 241
+  },
+  {
+    "country": "Kenya",
+    "team-matches-played": 154,
+    "matches-won": 42,
+    "matches-lost": 107
+  },
+  {
+    "country": "Namibia",
+    "team-matches-played": 14,
+    "matches-won": 5,
+    "matches-lost": 9
+  },
+  {
+    "country": "Ireland",
+    "team-matches-played": 156,
+    "matches-won": 67,
+    "matches-lost": 78
+  },
+  {
+    "country": "Scotland",
+    "team-matches-played": 115,
+    "matches-won": 42,
+    "matches-lost": 66
+  },
+  {
+    "country": "Afghanistan",
+    "team-matches-played": 126,
+    "matches-won": 59,
+    "matches-lost": 63
+  }
+]
+@app.route('/')
+
+def index():
+    return 'Welcome'
+
+@app.route('/data', methods = ['GET'])
+def list():
+    return jsonify({'data': data})
+
+@app.route('/data/<string:country>', methods=['GET'])
+def single(country):
+    # str = json.dumps(data)
+    # country = json.loads(str)
+    return jsonify({'country':data[country]})
+
+@app.route('/data?sort=', methods = ['GET'])
+def sort():
+    sort = json.dumps(data, sort_keys=True)
+    # sort1 = json.dumps(data['matches-won'], sort_keys=True)
+    x = sort.sort(key = lambda x:x["matches-won"])
+    return x
+    
+# @app.route('/matches-won', methods=['GET'])
+# def filter(matches-won):
+#    won = data.query.filter(data)
+
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
